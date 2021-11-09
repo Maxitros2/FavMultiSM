997d487537a09c178bd1f001d315b4512b7ebcb9
maxitros <maxim112111@gmail.com>
2021-11-10 02:36:02 +0300

    Rework Vk

--- FavMultiSM/Api/Socials/VKSender.cs
+++ FavMultiSM/Api/Socials/VKSender.cs
@@ -4,6 +4,10 @@ using FavMultiSM.Registration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using VkNet.Abstractions;
 using VkNet.Model.Attachments;
@@ -30,8 +34,18 @@ namespace FavMultiSM.Api.Socials
                 vkMessage.Message = message.Text;
             else
                 vkMessage.Message = " ";
-            //if (message.Attachments != null)
-              //  vkMessage.Attachments = message.Attachments.Select(x => new Photo() { PhotoSrc = new Uri(x) });
+            if (message.Attachments != null)
+            {
+                var uploadServer = await VkApi.Photo.GetMessagesUploadServerAsync(Convert.ToInt64(data.VKId));
+                var attachmentsList = new List<MediaAttachment>();
+                foreach (var photo in message.Attachments)
+                {
+                    var response = await UploadFile(uploadServer.UploadUrl, photo, photo.Split(".").Last());
+                    var attachment =  await VkApi.Photo.SaveMessagesPhotoAsync(response);
+                    attachmentsList.Add(attachment.First());
+                }
+                vkMessage.Attachments = attachmentsList;
+            }             
             await VkApi.Messages.SendAsync(vkMessage);
             return true;
         }
@@ -39,6 +53,30 @@ namespace FavMultiSM.Api.Socials
         public Task<bool> SendMessage(ReSendMessage message, UserAccountData data, DateTime start, DateTime end)
         {
             throw new NotImplementedException();
+        }
+        private async Task<byte[]> GetBytes(string fileUrl)
+        {
+            using (var webClient = new WebClient())
+            {
+                return await webClient.DownloadDataTaskAsync(fileUrl);
+            }
+        }
+        private async Task<string> UploadFile(string serverUrl, string file, string fileExtension)
+        {
+            // Получение массива байтов из файла
+            var data = await GetBytes(file);
+
+            // Создание запроса на загрузку файла на сервер
+            using (var client = new HttpClient())
+            {
+                var requestContent = new MultipartFormDataContent();
+                var content = new ByteArrayContent(data);
+                content.Headers.ContentType = MediaTypeHeaderValue.Parse("multipart/form-data");
+                requestContent.Add(content, "file", $"file.{fileExtension}");
+
+                var response = client.PostAsync(serverUrl, requestContent).Result;
+                return Encoding.Default.GetString(await response.Content.ReadAsByteArrayAsync());
+            }
         }
     }
 }
